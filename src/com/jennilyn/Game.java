dc26f9aad58ac0935a056af6c1aacb9ea67983aa
Jennilyn Howell <jennilynhowell@me.com>
2017-07-25 11:54:54 -0400

    Build startGame functionality

--- src/com/jennilyn/Game.java
+++ src/com/jennilyn/Game.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Game {
 
     List<Bike> bikes = new ArrayList<>();
+    private int finishLine = 100;
 
     public void addBike(){
         Bike bike = new Bike();
@@ -18,21 +19,51 @@ public class Game {
         }
     }
 
+    public void startRace() {
+        boolean raceActive = true;
+        List<Bike> finishers = new ArrayList<>();
+        while (raceActive) {
+            moveBikes();
+            for (Bike bike : bikes) {
+                if (bike.getLocation() >= finishLine) {
+                    finishers.add(bike);
+                    raceActive = false;
+                }
+            }
+        }
+        if (finishers.size() > 1) {
+            System.out.println("**********");
+            System.out.println("We have a photo finish!");
+            for (Bike finisher : finishers) {
+                finisher.report();
+            }
+        } else {
+            System.out.println("**********");
+            System.out.println("We have a Winner!");
+            finishers.get(0).report();
+        }
+
+    }
+
+    public void setFinishLine(int newFinishLine) {
+        finishLine = newFinishLine;
+    }
+
     public void bikeReport() {
         for (Bike bike : bikes) {
             bike.report();
         }
     }
 
-    public void startRace() {
+    public void moveBikes() {
         for (Bike bike : bikes) {
             bike.accelAndMove();
         }
     }
 
-    public void startRace(int distance){
+    public void moveBikes(int count){
         for (Bike bike : bikes) {
-            bike.accelAndMove(distance);
+            bike.accelAndMove(count);
         }
     }
 
--- src/com/jennilyn/Main.java
+++ src/com/jennilyn/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Game game = new Game();
+        game.setFinishLine(1000);
         game.addBike(5);
-        game.startRace(100);
+        game.startRace();
         game.bikeReport();
 
     }
